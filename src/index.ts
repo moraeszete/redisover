d0545b36b1751ce225dbd97fe501588b1b48abd5
moraeszete <lucas.moraes24536@gmail.com>
2025-04-11 23:11:02 -0300

    improving imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import { RedisOver as RedisOverClass } from './RedisOver';
+
+// Export for ESM/TS users
+export const RedisOver = RedisOverClass;
 export default RedisOverClass;
+
+// Export for CommonJS users
+if (typeof module !== 'undefined') {
+  module.exports = RedisOverClass;
+}
+ 
